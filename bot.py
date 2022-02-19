bc1562fdac637ff4b3c611df0d15739529e0fed2
storyknight <realstoryknight@gmail.com>
2022-02-19 00:12:37 -0600

    add !help and !m1 commands

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import discord
 import json
 import requests
 
-from private.config import token # personalized token
+from private.config import token  # personalized token
 from discord.ext import commands, tasks
 
 # read about documentation: https://discordpy.readthedocs.io/en/
@@ -15,9 +15,11 @@ intents.presences = False
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
 
 @bot.command(name="shrine")
 async def get_shrine(ctx):
@@ -31,5 +33,16 @@ async def get_shrine(ctx):
         shrine.append(perk["id"])
 
     await ctx.send(", ".join(shrine))
+
+
+@bot.command(name="m1")
+async def m1(ctx):
+    await ctx.send("TheBurntCroissant has m1'd more times than there are atoms in the universe.")
+
+
+@bot.command(name="help")
+async def get_shrine(ctx):
+    await ctx.send("Valid commands: 'm1', 'shrine'.")
+
 
 bot.run(token)
